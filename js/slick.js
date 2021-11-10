07445ae213a42726153e7605275e156c0ab9bc84
dykyys <dykyy.s@gmail.com>
2021-11-10 22:02:13 +0200

    completed

---
+++ js/slick.js
@@ -0,0 +1,29 @@
+$(".projects__list").slick({
+  centerMode: true,
+  autoplay: true,
+  centerPadding: "60px",
+  slidesToShow: 1,
+  infinite: true,
+  fade: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
